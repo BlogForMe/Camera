5a5141e5761bf9492b16256909e1df4829be6246
Jon <huaiyiheyuan@gmail.com>
2018-03-25 22:52:58 +0800

    servlet

--- src/com/jonzhou/servlet/CameraServlet.java
+++ src/com/jonzhou/servlet/CameraServlet.java
@@ -51,7 +51,7 @@ public class CameraServlet extends HttpServlet {
                     System.out.println(inputStream2String(is));
                 } else if (name.contains("img")) {
                     try {
-                        path = upload + item.getName() +"mine.jpg";
+                        path = upload + item.getName() ;
                         inputStream2File(is, path);
                         break;
                     } catch (Exception e) {
